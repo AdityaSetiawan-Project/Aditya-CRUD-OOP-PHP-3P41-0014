a1da58e90121688d500e5a2d294938b6e2ab22df
AdityaSetiawan-Project <adityasetiawan1005@gmail.com>
2024-10-19 18:15:32 +0700

    proses.php

--- proses.php
+++ proses.php
@@ -6,15 +6,15 @@ $db = new Database();
 $aksi = $_GET['aksi'];
 
 if ($aksi == "tambah") {
-    $db->tambahData($_POST['nama'], $_POST['alamat'], $_POST['nohp']);
+    $db->tambahData($_POST['nama'], $_POST['alamat'], $_POST['nohp'], $_POST['gender']);
     $_SESSION['message'] = "Data berhasil ditambahkan!";
-    $_SESSION['msg_type'] = "success";
-    header("Location: index.php");
+    $_SESSION['msg_type'] = "danger";
+    header("Location:index.php?status=success");
 } elseif ($aksi == "update") {
-    $db->updateData($_POST['id'], $_POST['nama'], $_POST['alamat'], $_POST['nohp']);
-    $_SESSION['message'] = "Data berhasil diupdate!";
-    $_SESSION['msg_type'] = "warning";
-    header("Location: index.php");
+    $db->updateData($_POST['id'], $_POST['nama'], $_POST['alamat'], $_POST['nohp'], $_POST['gender']);
+    $_SESSION['message'] = "Data berhasil diedit!";
+    $_SESSION['msg_type'] = "success";
+    header("Location:index.php?status=success");
 } elseif ($aksi == "hapus") {
     $db->hapusData($_GET['id']);
     $_SESSION['message'] = "Data berhasil dihapus!";
